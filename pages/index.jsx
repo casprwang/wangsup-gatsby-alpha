5f402c9bb553ae9eb24cdc37d83bbc7fc1d17740
wangsongiam <wangsongiam@gmail.com>
2017-05-15 20:32:16 -0400

    tablized

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,34 +7,39 @@ import { config } from 'config';
 export default class Index extends React.Component {
   render() {
     return (
-      <div className="main">
-        <div className="main-container">
-          <Helmet title={config.siteTitle} meta={[{ 'name': 'description', 'content': 'sample' }]} />
+    <div className="tabel-container">
+      <div className="table-cell">
 
-          <div className="main-container-title">
-            <h1>{config.siteTitle}</h1>
+        <div className="main">
+          <div className="main-container">
+            <Helmet title={config.siteTitle} meta={[{ 'name': 'description', 'content': 'sample' }]} />
+
+            <div className="main-container-title">
+              <h1>{config.siteTitle}</h1>
+            </div>
+
+            <div className="main-container-subtitle">
+              <h3>{config.siteDescription}</h3>
+            </div>
+
+            <div className="main-container-links">
+              <ul>
+                <li>
+                  <Link to={prefixLink('/philosophy/')}>Philosophy</Link>
+                </li>
+                <li>
+                  <Link to={prefixLink('/articles/')}>Articles</Link>
+                </li>
+                <li>
+                  <a href="https://github.com/bntzio/gatsby-starter-alchemy" target="_blank">Github</a>
+                </li>
+              </ul>
+            </div>
           </div>
 
-          <div className="main-container-subtitle">
-            <h3>{config.siteDescription}</h3>
-          </div>
-
-          <div className="main-container-links">
-            <ul>
-              <li>
-                <Link to={prefixLink('/philosophy/')}>Philosophy</Link>
-              </li>
-              <li>
-                <Link to={prefixLink('/articles/')}>Articles</Link>
-              </li>
-              <li>
-                <a href="https://github.com/bntzio/gatsby-starter-alchemy" target="_blank">Github</a>
-              </li>
-            </ul>
-          </div>
         </div>
-
       </div>
+    </div>
     );
   }
 }
